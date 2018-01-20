e1f524a842f790be3a71aa9c8f7241f96640d865
ElenaOl <pravutiner@gmail.com>
2018-01-20 11:14:01 -0800

    improving split sum, to any position of max num

--- split-sum-from-elements.js
+++ split-sum-from-elements.js
@@ -2,8 +2,9 @@
 
 //if array sorted and contains the number that is the sum of all others this number will be last.
 //if array not sorted we can find maximum and sum of elements and check if sum === 2*max num O(n) solution.
+//memory it is taken two arr, sum, max, indexOfMax.
 
-var arr = [1,2,3,4,5,15];
+var arr = [1,2,3,4,15,5];
 var arr1 = [];
 var sum = 0;
 var max = arr[0];
@@ -14,13 +15,15 @@ function split(arr){
   for(var i=0; i<arr.length-1; i++){
     if(arr[i] > arr[i+1]){
       max = arr[i];
+      indexOfMax = i;
     }else{
       max = arr[i+1];
+      indexOfMax = i+1;
     }
   }
   if(sum === 2*max){
-    arr.pop(max);
     arr1.push(max);
+    arr.splice(indexOfMax, 1);
     return "the sum of n-1 is " + arr1 + " the n-1 elements are " + arr;
   }else{
     return "there is no element that equals the sum of others found";
